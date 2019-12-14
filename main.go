1849c8985d3e0c54e619ac86e6460e694d250eb6
u1and0 <e01.ando60@gmail.com>
2019-12-15 07:57:52 +0900

    print datetime

--- main.go
+++ main.go
@@ -4,9 +4,20 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
 const filename = "data/11211002.DAT"
+
+// type Row struct {
+// 	Key uint16
+// 	Val uint16
+// }
+
+// func (r Row) String() string {
+// 	return fmt.Sprintf("(%s: %v)", string(r.Key), r.Val)
+// }
 
 func main() {
 	fp, err := os.Open(filename)
@@ -16,6 +27,8 @@ func main() {
 	defer fp.Close()
 
 	buf := make([]byte, 324) // 1秒あたり324Byte記録されている
+	// row := Row{}
+
 	for {
 		n, err := fp.Read(buf)
 		if n == 0 {
@@ -25,7 +38,29 @@ func main() {
 			panic(err)
 		}
 
+		// var i uint32
+
+		// binary.LittleEndian.PutUint32(buf, i)
+		// fmt.Printf("%x", binary.BigEndian.Uint32(buf)) // タイムスタンプしか出力されない
+		// binary.Read(bytes.NewBuffer(buf), binary.BigEndian, &i)
+
 		enco := hex.EncodeToString(buf)
-		fmt.Printf("%s\n", enco)
+
+		var y, m, d, H, M, S int
+		y, err = strconv.Atoi(enco[2:4])
+		m, err = strconv.Atoi(enco[0:2])
+		mm := time.Month(m) // 月のみMonth型をDate()関数に渡さなければならない
+		d, err = strconv.Atoi(enco[6:8])
+		H, err = strconv.Atoi(enco[4:6])
+		M, err = strconv.Atoi(enco[10:12])
+		S, err = strconv.Atoi(enco[8:10])
+		if err != nil {
+			fmt.Println(err)
+		}
+		tm := time.Date(2000+y, mm, d, H, M, S, 0, time.Local)
+		// fmt.Printf("%d%d%d\n", y, m, d)
+
+		// fmt.Printf("%s\n", enco)
+		fmt.Printf("%v\n", tm)
 	}
 }
