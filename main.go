5e729aece980673a3f33dd66076fad76cc07e056
u1and0 <e01.ando60@gmail.com>
2019-12-15 17:11:09 +0900

    [add] TransTemp() 温度変換関数

--- main.go
+++ main.go
@@ -27,8 +27,6 @@ func main() {
 	defer fp.Close()
 
 	buf := make([]byte, 324) // 1秒あたり324Byte記録されている
-	// row := Row{}
-
 	for {
 		n, err := fp.Read(buf)
 		if n == 0 {
@@ -38,26 +36,30 @@ func main() {
 			panic(err)
 		}
 
-		// var i uint32
-
-		// binary.LittleEndian.PutUint32(buf, i)
-		// fmt.Printf("%x", binary.BigEndian.Uint32(buf)) // タイムスタンプしか出力されない
-		// binary.Read(bytes.NewBuffer(buf), binary.BigEndian, &i)
-
 		enco := hex.EncodeToString(buf)
-		// fmt.Printf("%d%d%d\n", y, m, d)
 
-		tm, err := transTime(enco)
+		// /* 日時変換 */
+		tm, err := TransTime(enco)
 		if err != nil {
 			fmt.Println(err)
 		}
+
+		/* 温度変換 */
+		tmp, err := TransTemp(enco)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		/* 出力 */
 		// fmt.Printf("%s\n", enco)
 		fmt.Printf("%v\n", tm)
+		fmt.Printf("%f\n", tmp)
+		// fmt.Printf("%v\n", enco)
 	}
 }
 
-// 一秒分324文字列から日付・時間の変換
-func transTime(s string) (time.Time, error) {
+// TransTime : 一秒分324文字列から日付・時間の変換
+func TransTime(s string) (time.Time, error) {
 	y, err := strconv.Atoi(s[2:4])
 	m, err := strconv.Atoi(s[0:2])
 	mm := time.Month(m) // 月のみMonth型をDate()関数に渡さなければならない
@@ -67,4 +69,11 @@ func transTime(s string) (time.Time, error) {
 	S, err := strconv.Atoi(s[8:10])
 	tm := time.Date(2000+y, mm, d, H, M, S, 0, time.Local)
 	return tm, err
+}
+
+// TransTemp : 一秒分324文字列から温度の変換
+func TransTemp(s string) (float64, error) {
+	t, err := strconv.ParseInt(s[14:16]+s[12:14], 16, 0) // 16->10進数化
+	tmp := -45 + 175*float64(t)/65535
+	return tmp, err
 }
