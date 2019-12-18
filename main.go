b81365dc978e8031ac336fc8c1b211b46029f7ca
u1and0 <e01.ando60@gmail.com>
2019-12-19 06:46:38 +0900

    [mod] 加速度計算, 負の値が出るように修正 @main.go/TransAcc()

--- main.go
+++ main.go
@@ -91,21 +91,26 @@ func main() {
 		// fmt.Printf("JSON:%v\n", d)
 		je, _ := json.MarshalIndent(d, "", "\t")
 		js = append(js, string(je))
-		fmt.Printf("%s\n", js)
 	}
+	fmt.Printf("%s\n", js)
 }
 
 // TransAcc : 一秒分324文字列から加速度Xの変換
 func TransAcc(s, xyz string) ([]float64, error) {
 	var (
-		acci int64 // バイトから読み取った文字列から変換したint加速度
+		accu uint64 // バイトから読み取った文字列から変換したint加速度
 		err  error
 		acxl []float64                                                  // 加速度配列
 		axis map[string]int = map[string]int{"x": 48, "y": 52, "z": 56} // 方向xyz
 	)
 	for i := axis[xyz]; i < len(s); i += 12 { // 初期バイトはxyzの方向による
 		ss := s[i+2:i+4] + s[i:i+2]
-		acci, err = strconv.ParseInt(ss, 16, 0)        // 16->10進数化
+		accu, err = strconv.ParseUint(ss, 16, 0) // 16->10進数化
+		if err != nil {
+			fmt.Println(err)
+		}
+		acci := int16(accu) // 一気にfloatに渡してはいけない
+		// なぜなら、Uintの補数をintとして換算しないと小数点計算されてしまう
 		acxl = append(acxl, 16000*float64(acci)/32768) // 換算加速度を加速度配列に格納
 	}
 	return acxl, err
